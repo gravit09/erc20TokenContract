6f908597fb1b5ad5167270bd4ee00459c62313b2
gravit09 <Garvitchoudhary09@gmail.com>
2024-02-12 00:13:49 +0530

    code

--- coin.sol
+++ coin.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
-//
+
 import "@openzeppelin/contracts@5.0.1/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts@5.0.1/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts@5.0.1/token/ERC20/extensions/ERC20Pausable.sol";
